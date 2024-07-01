2b7d47280f0196927cfd4bf4df5c76a378174e32
Juan Pablo Quiroga <juanpablo@harbor.co>
2024-07-01 14:47:21 -0400

    Revert "fix(cicd): enable --no-frozen-lockfile"
    This reverts commit 7097ee08377d933000bb92b06df1f3488f150da0.

--- scripts/deploy-harbor-production.sh
+++ scripts/deploy-harbor-production.sh
@@ -23,7 +23,7 @@ echo "pnpm version: $(pnpm --version)"
 
 # Install dependencies
 echo "Installing dependencies..."
-pnpm install --no-frozen-lockfile
+pnpm install
 
 
 # Build the telemed-ehr/app
